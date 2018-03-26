fc5a71376e2df9f24d98bf49636cca071cf4a204
InTheHeartbeat <intheheartbeat0@gmail.com>
2018-03-26 08:35:34 +0300

    Implemented EmtyCellToBrush converter #4

---
+++ Mp3Tagger/Mp3Tagger/Converters/EmptyCellToBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Mp3Tagger.Converters
+{
+    public class EmptyCellToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (String.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return Brushes.BlueViolet;
+            }
+            return Application.Current.Resources["DataGridBackground"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
